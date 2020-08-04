2f76180aec3a8bdce47b9870875787a28d33c028
liqm <liqm@lenovocloud.com>
2020-08-04 17:18:20 +0800

    dev: 使用vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import {createRouter, createWebHistory} from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const router  =   createRouter({
+    history: createWebHistory(),
+    routes: [{
+        path: '/',
+        component:HelloWorld,
+    }],
+})
+app.use(router)
+app.mount('#app')
