8c1d0c30f7e25eac20ee984b309b3dea9f6f757f
liqm <liqm@lenovocloud.com>
2020-08-04 18:02:05 +0800

    dev: js文件改成ts

---
+++ router.ts
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HelloWorld from './src/components/HelloWorld.vue'
+import Home from './src/views/Home.vue'
+import Doc from './src/views/Doc.vue'
+
+export const router  =   createRouter({
+    history: createWebHistory(),
+    routes: [{
+        path: '/',
+        component:Home,
+    },{
+        path: '/doc',
+        component: Doc
+    }],
+})
