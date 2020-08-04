666dae8aec7c6bf00741254caa7b31be9bbcb9b9
liqm <liqm@lenovocloud.com>
2020-08-04 18:02:30 +0800

    dev: 新增home和doc

---
+++ src/views/Doc.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        我是文档
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="banner">
+        <h1>vue3-ui</h1>
+        <h2>基于vue3的组件库</h2>
+        <p class="actions">
+            <a href="https://github.com">GitHub</a>
+            <router-link to="/doc">开始</router-link>
+        </p>
+    </div>
+</template>
+<script lang="ts">
+
+</script>
+<style lang="scss" scoped>
+
+</style>
